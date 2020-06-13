f856e25be65f50154ddc59de4ff307908b4375d8
Artiom <artiomponomariovcode@gmail.com>
2020-06-13 06:59:37 +0300

    added custom select support

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,6 @@
 import main from './pages/main';
 import header from './modules/header/';
+import Choices from "choices.js";
 import setPageHeight from './modules/setPageHeight';
 
 const global = () => {
@@ -9,7 +10,7 @@ const global = () => {
 
 const router = {
   pages: {
-    main: main,
+    main: main
   },
   global: global,
 };
@@ -29,3 +30,78 @@ router.global();
 if (route) {
   router.pages[route]();
 }
+
+let os = "Not known";
+
+if (
+  os === "Not known" ||
+  navigator.appVersion.indexOf("Mac") !== -1 ||
+  (navigator.appVersion.indexOf("Android") !== -1
+    && navigator.appVersion.indexOf("Linux") !== -1)
+) {
+  document.getElementById('macos-hero').classList.add("is-visible");
+  document.getElementById('macos-col').classList.remove("is-visible");
+} else if (navigator.appVersion.indexOf("Win") !== -1) {
+  os = "Windows OS";
+  document.getElementById('windows-hero').classList.add("is-visible");
+  document.getElementById('windows-col').classList.remove("is-visible");
+} else if (navigator.appVersion.indexOf("Android") !== -1) {
+  os = "Android OS";
+  document.getElementById('android-hero').classList.add("is-visible");
+  document.getElementById('android-col').classList.remove("is-visible");
+} else if (navigator.appVersion.indexOf("Linux") !== -1) {
+  os = "Linux OS";
+  document.getElementById('linux-hero').classList.add("is-visible");
+  document.getElementById('linux-col').classList.remove("is-visible");
+} else if (navigator.appVersion.indexOf("iOS") !== -1) {
+  os = "iOS OS";
+  document.getElementById('linux-hero').classList.add("is-visible");
+  document.getElementById('linux-col').classList.remove("is-visible");
+}
+
+console.log(JSON.stringify(os));
+console.log(JSON.stringify(navigator.appVersion));
+
+const choiceOptions = {
+  searchEnabled: false,
+  itemSelectText: 'Press to download',
+  classNames: {
+    containerOuter: 'card__select select select--choice select--dark choices',
+  }
+};
+
+const windowsChoiceSelector = document.getElementById('choice-windows');
+const linuxChoiceSelector = document.getElementById('choice-linux');
+const windowsChoice = new Choices(windowsChoiceSelector, choiceOptions);
+const linuxChoice = new Choices(linuxChoiceSelector, choiceOptions);
+
+windowsChoiceSelector.addEventListener('choice', function (event) {
+  console.log(event);
+  let a = document.createElement('a');
+  document.body.appendChild(a);
+  a.download = "file test";
+  a.href = "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAOCAYAAAAmL5yKAAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAAAJcEhZcwAADsQAAA7EAZUrDhsAAABWSURBVDhPY0xISPh//0UOA7mAiVyNMH2jBjAwkBQGjD9KGBTEJ6OEO0kG2NvbMwCjnXwDsEU5SS5ANuDhjRCGJbPFSQsDdBfIyMhQZgDIQLK9QLWkDABPsQw5I+5qmAAAAABJRU5ErkJggg==";
+  a.click();
+  a.parentNode.removeChild(a);
+});
+
+linuxChoiceSelector.addEventListener('choice', function (event) {
+  console.log(event);
+  let a = document.createElement('a');
+  document.body.appendChild(a);
+  a.download = "event";
+  a.href = "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAOCAYAAAAmL5yKAAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAAAJcEhZcwAADsQAAA7EAZUrDhsAAABWSURBVDhPY0xISPh//0UOA7mAiVyNMH2jBjAwkBQGjD9KGBTEJ6OEO0kG2NvbMwCjnXwDsEU5SS5ANuDhjRCGJbPFSQsDdBfIyMhQZgDIQLK9QLWkDABPsQw5I+5qmAAAAABJRU5ErkJggg==";
+  a.click();
+  a.parentNode.removeChild(a);
+});
+
+windowsChoiceSelector.addEventListener('choice', function (event) {
+  console.log(event);
+  let a = document.createElement('a');
+  document.body.appendChild(a);
+  a.download = "event";
+  a.href = "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAOCAYAAAAmL5yKAAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAAAJcEhZcwAADsQAAA7EAZUrDhsAAABWSURBVDhPY0xISPh//0UOA7mAiVyNMH2jBjAwkBQGjD9KGBTEJ6OEO0kG2NvbMwCjnXwDsEU5SS5ANuDhjRCGJbPFSQsDdBfIyMhQZgDIQLK9QLWkDABPsQw5I+5qmAAAAABJRU5ErkJggg==";
+  a.click();
+  a.parentNode.removeChild(a);
+});
+

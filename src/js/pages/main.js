d4f232ca3b84b1938f002f6e7c3a46ae94bf5a98
Artiom <artiomponomariovcode@gmail.com>
2020-06-29 19:13:41 +0300

    video temporary fix

--- src/js/pages/main.js
+++ src/js/pages/main.js
@@ -26,45 +26,43 @@ const main = () => {
   // console.log(tabsDesktop);
   // console.log(tabsMobile);
   // console.log(videos);
-
+  for (let y = 0; y < 5; y++) {
+    tabsDesktop[y].addEventListener('click', function () {
+      videos[y].play();
+    });
+  }
+  for (let j = 0; j < 5; j++) {
+    tabsMobile[j].addEventListener('click', function () {
+      videos[j].play();
+    });
+  }
   function checkScroll() {
-    for (let y = 0; y < 5; y++) {
-      tabsDesktop[y].addEventListener('click', function () {
-        videos[y].play();
-      });
-    }
-    for (let j = 0; j < 5; j++) {
-      tabsMobile[j].addEventListener('click', function () {
-        videos[j].play();
-      });
-    }
-
-    for (let i = 0; i < videos.length; i++) {
-      if (i === 0 || i === 5 || i === 6) {
-        let
-          x = videos[i].offsetLeft,
-          y = videos[i].offsetTop,
-          w = videos[i].offsetWidth,
-          h = videos[i].offsetHeight,
-          r = x + w,
-          b = y + h,
-          visibleX,
-          visibleY,
-          visible;
-
-        visibleX = Math.max(0, Math.min(w, window.pageXOffset + window.innerWidth - x, r - window.pageXOffset));
-        visibleY = Math.max(0, Math.min(h, window.pageYOffset + window.innerHeight - y, b - window.pageYOffset));
-
-        visible = visibleX * visibleY / (w * h);
-
-        // console.log('video:', i);
-        // console.log(visibleY);
-        // console.log('______________');
-        if (0 < visible < 100) {
-          videos[i].play();
-        }
-      }
-    }
+    // for (let i = 0; i < videos.length; i++) {
+    //   if (i === 0 || i === 5 || i === 6) {
+    //     let
+    //       x = videos[i].offsetLeft,
+    //       y = videos[i].offsetTop,
+    //       w = videos[i].offsetWidth,
+    //       h = videos[i].offsetHeight,
+    //       r = x + w,
+    //       b = y + h,
+    //       visibleX,
+    //       visibleY,
+    //       visible;
+    //
+    //     visibleX = Math.max(0, Math.min(w, window.pageXOffset + window.innerWidth - x, r - window.pageXOffset));
+    //     visibleY = Math.max(0, Math.min(h, window.pageYOffset + window.innerHeight - y, b - window.pageYOffset));
+    //
+    //     visible = visibleX * visibleY / (w * h);
+    //
+    //     // console.log('video:', i);
+    //     // console.log(visibleY);
+    //     // console.log('______________');
+    //     if (0 < visible < 100) {
+    //       videos[i].play();
+    //     }
+    //   }
+    // }
   }
 
   document.addEventListener('scroll', checkScroll, false);

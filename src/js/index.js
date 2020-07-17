4de2e65c0c3d5b58649a316b20629b99c181c79c
George <t.georgit@yandex.ru>
2020-07-17 17:52:56 +0300

    service worker in index

--- src/js/index.js
+++ src/js/index.js
@@ -4,6 +4,17 @@ import download from './pages/download';
 import header from './modules/header/';
 import setPageHeight from './modules/setPageHeight';
 
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.getRegistrations().then(function (registrations) {
+    for (let registration of registrations) {
+      //unregister service worker for old domain
+      registration.unregister()
+    }
+  }).catch(function (err) {
+    // fail state, which is fine as we just don't want a service worker.
+    console.log('Fail: ', err);
+  });
+}
 
 const global = () => {
   setPageHeight();
